922a03f60ddeee670a49983b09c1be958b330f32
Michal Duda <duda@ntb104614.lan>
2022-02-19 11:23:02 +0100

    🔩Code: Docker customization

--- docker/docker-entry.sh
+++ docker/docker-entry.sh
@@ -4,17 +4,19 @@
 
 set -e
 
+readonly ffmpeg_version=n5.0
+
 OUTPUTDIR=/output
 
-./cross_compile_ffmpeg.sh --build-ffmpeg-shared=y --build-ffmpeg-static=y --disable-nonfree=n --build-intel-qsv=y --compiler-flavors=win64 --enable-gpl=y
+./cross_compile_ffmpeg.sh --ffmpeg-git-checkout-version=$ffmpeg_version --build-amd-amf=n --build-ffmpeg-static=y --disable-nonfree=n --compiler-flavors=win64 --enable-gpl=y
 
 mkdir -p $OUTPUTDIR/static/bin
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffmpeg.exe $OUTPUTDIR/static/bin
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffprobe.exe $OUTPUTDIR/static/bin
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac/ffplay.exe $OUTPUTDIR/static/bin
+cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_$ffmpeg_version/ffmpeg.exe $OUTPUTDIR/static/bin
+cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_$ffmpeg_version/ffprobe.exe $OUTPUTDIR/static/bin
+cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_$ffmpeg_version/ffplay.exe $OUTPUTDIR/static/bin
 
-mkdir -p $OUTPUTDIR/shared
-cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_shared/bin/ $OUTPUTDIR/shared
+#mkdir -p $OUTPUTDIR/shared
+#cp -R -f ./sandbox/win64/ffmpeg_git_with_fdk_aac_shared/bin/ $OUTPUTDIR/shared
 
 if [[ -f /tmp/loop ]]; then
   echo 'sleeping forever so you can attach to this docker if desired' # without this if there's a build failure the docker exits and can't get in to tweak stuff??? :|
